cdf69622adf273c72eea044e919e018358ce7bbc
Huzaifa Rashid <huzaifaras10@gmail.com>
2026-03-02 13:25:23 -0800

    .

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",  # <-- Add this first
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Add this first
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 ROOT_URLCONF = 'core.urls'
