2cca31f5090d08c49f52c1331be76e5fc5647057
Saad Jamil <saadjamil504@gmail.com>
2026-03-03 23:19:18 +0500

    new

--- backend/expenses/models.py
+++ backend/expenses/models.py
@@ -7,6 +7,15 @@ class Expense(models.Model):
         on_delete=models.CASCADE,
         related_name="expenses"
     )
+
+    CATEGORY_CHOICES = [
+        ('Food', 'Food'),
+        ('Transport', 'Transport'),
+        ('Shopping', 'Shopping'),
+        ('Bills', 'Bills'),
+        ('Other', 'Other'),
+    ]
+
     title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
