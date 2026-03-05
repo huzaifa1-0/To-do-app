ec0fa3655a50266de0f0a65c0a90f3f756279e0b
Huzaifa Rashid <huzaifaras10@gmail.com>
2026-03-05 09:52:58 -0800

    .

--- backend/manage.py
+++ backend/manage.py
@@ -4,10 +4,10 @@ import os
 import sys
 
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,10 +16,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
-    # 👇 ADD THIS LINE HERE
-    import create_superuser
-
     execute_from_command_line(sys.argv)
 
 
